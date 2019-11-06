315701c8e26b7465ceea25cad924527b38a45397
ckarrie <ckarrie@localhost>
2019-11-06 10:16:53 +0100

    add archive

--- csgomatches/urls.py
+++ csgomatches/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view()),
+    path('', views.IndexView.as_view(), name="match_upcoming"),
+    path('archive/<int:year>/', views.YearArchiveView.as_view(), name="match_history"),
     path('admin/', admin.site.urls),
 ]
--- csgomatches/views.py
+++ csgomatches/views.py
@@ -13,4 +13,19 @@ class IndexView(generic.ListView):
         qs = qs.filter(first_map_at__date__gte=timezone.now().date()).order_by('first_map_at')
         return qs
 
+    def get_context_data(self, *args, **kwargs):
+        ctx = super(IndexView, self).get_context_data(*args, **kwargs)
+        ctx.update({
+            'date_list': self.model.objects.all().dates('first_map_at', 'year'),
+            'current_view': 'index'
+        })
+        return ctx
+
+
+class YearArchiveView(generic.YearArchiveView):
+    model = models.Match
+    date_field = 'first_map_at'
+    make_object_list = True
+    allow_future = False
+
 
