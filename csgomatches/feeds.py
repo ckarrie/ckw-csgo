f6e32c185086f4208ded2aaba121181cdd1c5d46
ckarrie <ckarrie@localhost>
2019-11-12 12:49:57 +0100

    add google index

--- csgomatches/feeds.py
+++ csgomatches/feeds.py
@@ -1,6 +1,10 @@
-from django_ical.views import ICalFeed
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
 from django.utils import timezone
+from django_ical.views import ICalFeed
+
 from . import models
+
 
 class UpcomingEventsFeed(ICalFeed):
     """
@@ -28,3 +32,34 @@ class UpcomingEventsFeed(ICalFeed):
         return '#matchup-{}'.format(item.pk)
 
 
+class UpcomingMatchesSitemap(Sitemap):
+    changefreq = "hourly"
+    priority = 1.0
+
+    def items(self):
+        return models.Match.objects.filter(
+            first_map_at__date__gte=timezone.now().date()
+        ).order_by('first_map_at')
+
+    def location(self, obj):
+        return '/#matchup-{}'.format(obj.pk)
+
+    def lastmod(self, obj):
+        return obj.first_map_at
+
+
+class ArchiveMatchesSitemap(Sitemap):
+    changefreq = "daily"
+    priority = 0.5
+
+    def items(self):
+        return models.Match.objects.filter(
+            first_map_at__date__lt=timezone.now().date()
+        ).order_by('-first_map_at')
+
+    def location(self, obj):
+        archive_url = reverse('match_history', kwargs={'year': obj.first_map_at.year})
+        return '{}#matchup-{}'.format(archive_url, obj.pk)
+
+    def lastmod(self, obj):
+        return obj.first_map_at
--- csgomatches/urls.py
+++ csgomatches/urls.py
@@ -1,17 +1,24 @@
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.cache import cache_page
-from django.conf import settings
+from django.contrib.sitemaps.views import sitemap
+from django.views.generic import TemplateView
 
-from . import views
 from . import feeds
+from . import views
 
 if settings.DEBUG:
     CACHE_INDEX_TIME = 0
     CACHE_ARCHIVE_TIME = 0
 else:
-    CACHE_INDEX_TIME = 1*60
-    CACHE_ARCHIVE_TIME = 10*60
+    CACHE_INDEX_TIME = 1 * 60       # 1 Minute
+    CACHE_ARCHIVE_TIME = 60 * 60    # 60 Minutes    1 Hour
+
+sitemaps = {
+    'upcoming': feeds.UpcomingMatchesSitemap,
+    'history': feeds.ArchiveMatchesSitemap,
+}
 
 urlpatterns = [
     path(
@@ -33,4 +40,13 @@ urlpatterns = [
         'admin/',
         admin.site.urls
     ),
+    path('sitemap.xml', cache_page(CACHE_ARCHIVE_TIME)(sitemap), {
+        'sitemaps': sitemaps,
+        'template_name': 'csgomatches/feeds/google-news.xml'
+    }, name='django.contrib.sitemaps.views.sitemap'),
+    path(
+        'robots.txt',
+        TemplateView.as_view(template_name="csgomatches/feeds/robots.txt", content_type="text/plain"),
+        name="robots_file"
+    )
 ]
