edb66417d8b2da9d2d837061c37b3e6d652c2dd6
ckarrie <ckarrie@localhost>
2019-11-22 22:02:42 +0100

    add websockets

--- setup.py
+++ setup.py
@@ -20,6 +20,6 @@ setup(
         'django',
         'requests',
         'psycopg2-binary', 'bs4', 'python-dateutil', 'python-memcached',
-        'django_ical', 'websocket_client', 'djangorestframework',
+        'django_ical', 'websocket_client', 'djangorestframework', 'websockets',
     ]
 )
