dcaf3ca235c1394adc4cb667451ff7ea08ff7863
ckarrie <ckarrie@localhost>
2019-12-10 21:27:39 +0100

    adding user-agent

--- csgomatches/utils/scrapers/esea.py
+++ csgomatches/utils/scrapers/esea.py
@@ -8,39 +8,43 @@ from django.apps import apps
 def get_bracket_match(bracket_id, match_id, update_id=None, reverse_score=False):
     esea_bracket_url = 'https://play.esea.net/index.php?s=events&d=brackets&id={}'.format(bracket_id)
     scraper = cfscrape.CloudflareScraper()
-    response = scraper.get(esea_bracket_url)
+    response = scraper.get(
+        esea_bracket_url,
+        headers={'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36'}
+    )
     print("Got page from", esea_bracket_url, response.status_code)
-    soup = BeautifulSoup(response.text, "html.parser")
-    match_div = soup.find('div', {'id': 'match-modal-{}'.format(match_id)})
-    map_name = match_div.find("table", {'class': 'match-meta'}).select("td")[1].text
-    print("Map", map_name)
-    scores_table = match_div.find("table", {'class': 'scores'})
-    try:
-        team_a = scores_table.select("td")[0].text
-        score_a = scores_table.select("td")[1].text
-        team_b = scores_table.select("td")[2].text
-        score_b = scores_table.select("td")[3].text
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, "html.parser")
+        match_div = soup.find('div', {'id': 'match-modal-{}'.format(match_id)})
+        map_name = match_div.find("table", {'class': 'match-meta'}).select("td")[1].text
+        print("Map", map_name)
+        scores_table = match_div.find("table", {'class': 'scores'})
+        try:
+            team_a = scores_table.select("td")[0].text
+            score_a = scores_table.select("td")[1].text
+            team_b = scores_table.select("td")[2].text
+            score_b = scores_table.select("td")[3].text
 
-        print(team_a, score_a, " - ",team_b, score_b)
-    except IndexError:
-        return
+            print(team_a, score_a, " - ",team_b, score_b)
+        except IndexError:
+            return
 
 
-    if update_id:
-        match = apps.get_model('csgomatches.Match').objects.filter(id=update_id).first()
-        if match:
-            map_1 = match.matchmap_set.filter(map_nr=1).first()
-            if map_1:
-                if reverse_score:
-                    map_1.rounds_won_team_a = int(score_b)
-                    map_1.rounds_won_team_b = int(score_a)
-                else:
-                    map_1.rounds_won_team_a = int(score_a)
-                    map_1.rounds_won_team_b = int(score_b)
+        if update_id:
+            match = apps.get_model('csgomatches.Match').objects.filter(id=update_id).first()
+            if match:
+                map_1 = match.matchmap_set.filter(map_nr=1).first()
+                if map_1:
+                    if reverse_score:
+                        map_1.rounds_won_team_a = int(score_b)
+                        map_1.rounds_won_team_b = int(score_a)
+                    else:
+                        map_1.rounds_won_team_a = int(score_a)
+                        map_1.rounds_won_team_b = int(score_b)
 
-                map_1.save()
-        else:
-            print("Match not found")
+                    map_1.save()
+            else:
+                print("Match not found")
 
 
 def as_thread(bracket_id, match_id, update_id=None, reverse_score=False):
