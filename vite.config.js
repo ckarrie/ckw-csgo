05297d98b4702b43cd4f2b5b27ceec2fb834b04b
Paul Kamma <paul.kamma@gmail.com>
2025-03-20 22:30:35 +0100

    Update vite.config.js
    - Delete output asset folder

--- vite.config.js
+++ vite.config.js
@@ -1,42 +1,53 @@
 import { defineConfig, loadEnv } from 'vite';
 import { resolve, join } from 'path';
+import { rmSync } from 'fs';
 
-const postcssConfig = {
-  plugins: [
-  ],
-};
+const assetsDir = 'csgomatches/frontend';
 
-export default defineConfig((mode) => {
-  const env = loadEnv(mode, process.cwd(), '');
+const plugins = [
+	{
+		name: 'clean-assets-folder',
+		buildStart() {
+			const assetsPath = resolve(
+				__dirname,
+				'csgomatches/static',
+				assetsDir
+			);
+			rmSync(assetsPath, { recursive: true, force: true });
+		},
+	},
+];
 
-  const INPUT_DIR = './src';
-  const OUTPUT_DIR = './../ckw-csgo/csgomatches/static';
+export default defineConfig(mode => {
+	const env = loadEnv(mode, process.cwd(), '');
 
-  return {
-    plugins: [],
-    resolve: {
-      alias: {
-        '@': resolve(INPUT_DIR),
-      },
-    },
-    root: resolve(INPUT_DIR),
-    base: '/static/',
-    css: {
-    },
-    server: {
-      host: env.DJANGO_VITE_DEV_SERVER_HOST,
-      port: env.DJANGO_VITE_DEV_SERVER_PORT,
-    },
-    build: {
-      manifest: "manifest.json",
-      emptyOutDir: false,
-      outDir: resolve(OUTPUT_DIR),
-      assetsDir: "csgomatches/assets",
-      rollupOptions: {
-        input: {
-          css: join(INPUT_DIR, '/entry.js'),
-        },
-      },
-    },
-  };
+	const INPUT_DIR = './src';
+	const OUTPUT_DIR = './../ckw-csgo/csgomatches/static';
+
+	return {
+		plugins,
+		resolve: {
+			alias: {
+				'@': resolve(INPUT_DIR),
+			},
+		},
+		root: resolve(INPUT_DIR),
+		base: '/static/',
+		css: {},
+		server: {
+			host: env.DJANGO_VITE_DEV_SERVER_HOST,
+			port: env.DJANGO_VITE_DEV_SERVER_PORT,
+		},
+		build: {
+			manifest: 'manifest.json',
+			emptyOutDir: false,
+			outDir: resolve(OUTPUT_DIR),
+			assetsDir,
+			rollupOptions: {
+				input: {
+					css: join(INPUT_DIR, '/entry.js'),
+				},
+			},
+		},
+	};
 });
