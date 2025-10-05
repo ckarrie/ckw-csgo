578977e4297084314c0644cee85331672520e078
Ian Paul Falatik <63957260+ifalatik@users.noreply.github.com>
2025-10-05 15:44:50 +0200

    add verbose name for CsMatch

--- csgomatches/models/matches.py
+++ csgomatches/models/matches.py
@@ -2,6 +2,10 @@ from csgomatches.models.base_models import BaseOneOnOneMatch
 from csgomatches.models.global_models import Game
 
 class CsMatch(BaseOneOnOneMatch):
+    class Meta:
+        verbose_name = "CS Match"
+        verbose_name_plural = "CS Matches"
+
     def __str__(self):
         return f"{self.participant_1} vs {self.participant_2} on {self.starts_at}"
 
