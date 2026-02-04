57b4067fff0d18a9f5bc85f85b7fe500df14037a
ckarrie <christian@karrie.info>
2026-02-04 15:56:37 +0100

    redefine querys

--- csgomatches/drf_api/filters.py
+++ csgomatches/drf_api/filters.py
@@ -5,12 +5,12 @@ from django.apps import apps
 
 class MatchTeamFilter(django_filters.FilterSet):
     # Wir definieren einen künstlichen Parameter "team"
-    team = django_filters.CharFilter(method='filter_by_team', label="Filter by Team 1 or Team 2")
-    game = django_filters.CharFilter(field_name='lineup_a__game__name_short', lookup_expr='iexact', label="Game short name (cs, LoL, etc.)")
+    team = django_filters.CharFilter(method='filter_by_team', label="Filter by Team name", help_text="Examples: BIG, BIG Academy, BIG Equipa")
+    game = django_filters.CharFilter(field_name='lineup_a__game__name_short', lookup_expr='iexact', label="Filter by Game", help_text="Examples: cs (Counterstrike), LoL (League of Legends), TM (Trackmainia)")
 
     class Meta:
         model = apps.get_model('csgomatches.Match')
-        fields = ['lineup_a__team__name', 'lineup_a__game__name_short']
+        fields = []
 
     def filter_by_team(self, queryset, name, value):
         # Filtert in beiden Feldern gleichzeitig
